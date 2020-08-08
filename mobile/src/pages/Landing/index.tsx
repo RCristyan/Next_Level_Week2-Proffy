2193a12ccdc22aa2129325660f563ba19bf1caa9
RCristyan <rcristyan9@gmail.com>
2020-08-08 12:47:42 -0300

    Landing page mobile integrada ao Back-end.

--- mobile/src/pages/Landing/index.tsx
+++ mobile/src/pages/Landing/index.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { View, Image, Text, TouchableOpacity } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { View, Image, Text } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import { RectButton } from 'react-native-gesture-handler';
 
@@ -8,9 +8,18 @@ import landingImg from '../../images/landing.png';
 import studyIcon from '../../images/icons/study.png';
 import giveClassessIcon from '../../images/icons/give-classes.png';
 import heartIcon from '../../images/icons/heart.png';
+import api from '../../services/api';
 
 function Landing(){
     const { navigate } = useNavigation();
+    const [totalConnections, setTotalConnections] = useState(0);
+
+    useEffect(() => {
+        api.get('/connections').then(response => {
+            const total = response.data.total;
+            setTotalConnections(total);
+        })
+    }, []);
 
     function handleNavigationToGiveClassesPage(){
         navigate('GiveClasses');
@@ -50,7 +59,7 @@ function Landing(){
             </View>
 
             <Text style={ styles.totalConnections }>
-                Total de 200 conexões já realizadas {' '}
+                Total de { totalConnections } conexões já realizadas {' '}
                 <Image source={ heartIcon }/>
             </Text>
         </View>
